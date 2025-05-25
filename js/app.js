d4124667069e6cbf2ac268253b6b45e8695b55b3
jairo9991 <jaixx.102@gmail.com>
2025-05-25 05:34:16 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,66 @@
+let imagenes = [
+    {
+        "url": "img/p9.webp",
+        
+    },
+    {
+        "url": "img/p10.webp",
+    },
+    {
+        "url": "img/p1.webp",
+    },
+	   {
+        "url": "img/p2.webp",
+    },
+	   {
+        "url": "img/p3.webp",
+    },
+	   {
+        "url": "img/p4.webp",
+    }
+]
+
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let puntos = document.getElementById('puntos');
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+    actual -=1
+
+    if (actual == -1){
+        actual = imagenes.length - 1
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+adelante.addEventListener('click', function(){
+    actual +=1
+
+    if (actual == imagenes.length){
+        actual = 0
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i <imagenes.length; i++){
+      
+    } 
+}
